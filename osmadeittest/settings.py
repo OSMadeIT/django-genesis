55c52853f52937809cc22cb71994bd7eff08e34e
Mitch <mitchodongo@gmai.com>
2018-05-02 18:04:41 +0300

    add settings to help in deploying postgres to heroku

--- osmadeittest/settings.py
+++ osmadeittest/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import psycopg2
 
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -163,3 +167,6 @@ CKEDITOR_CONFIGS = {
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
